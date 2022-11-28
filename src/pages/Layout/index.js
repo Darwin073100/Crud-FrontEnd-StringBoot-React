b64930a9add9fdcaad9aaf120a9d19ce0f942ddd
Darwin073100 <edwinegq@outlook.com>
2022-11-28 17:06:29 -0600

    Menu del inicio

--- src/pages/Layout/index.js
+++ src/pages/Layout/index.js
@@ -1,18 +1,29 @@
 import React from "react";
 import { Outlet, Link } from "react-router-dom";
+import "../../styles/Home.css";
+import Student from '../../assets/persona.svg';
+import Inscriptionn from '../../assets/inscription.svg';
+import Career from '../../assets/carrera.svg';
+import Home from '../../assets/home.svg';
 
-function Layout(){
-    return(
-        <div className="comp-layout">
-            <ul>
-                <li><Link to="/">Home</Link></li>
-                <li><Link to="/persona">Persona</Link></li>
-                <li><Link to="/inscripcion">Inscripcion</Link></li>
-                <li><Link to="/carrera">Carrera</Link></li>
-            </ul>
-            <Outlet />
-        </div>
-    );
+function Layout() {
+  return (
+    <div className="comp-layout">
+      <Link className="item" to="/">
+        <img src={Home} alt="Home"/>
+      </Link>
+      <Link className="item" to="/persona">
+        <img src={Student} alt="Student"/>
+      </Link>
+      <Link className="item" to="/inscripcion">
+        <img src={Inscriptionn} alt="Inscription"/>
+      </Link>
+      <Link className="item" to="/carrera">
+        <img src={Career} alt="Career"/>
+      </Link>
+      <Outlet />
+    </div>
+  );
 }
 
 export { Layout };
